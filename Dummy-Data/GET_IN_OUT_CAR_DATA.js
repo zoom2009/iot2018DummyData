0260a90119eac577d059466b2755855060ff47cc
unknown <afumagic3@gmail.com>
2018-05-03 23:32:08 +0700

    dummy 30students, 30getInOutCar

--- Dummy-Data/GET_IN_OUT_CAR_DATA.js
+++ Dummy-Data/GET_IN_OUT_CAR_DATA.js
@@ -1,11 +1,427 @@
 var GET_IN_OUT_CAR_DATA = [
+    // ============= CAR 001 ==============
   {
     RFID: "rfid_dummy_001",
     carID: "car001",
     in_out: "i",
     stationID: "station001",
-    dateTime: "12-04-2018 10:42:58"
-  }
+    dateTime: "03-05-2018 10:00:00"
+  },
+  {
+    RFID: "rfid_dummy_001",
+    carID: "car001",
+    in_out: "o",
+    stationID: "station003",
+    dateTime: "03-05-2018 10:00:10"
+  },
+  {
+    RFID: "rfid_dummy_002",
+    carID: "car001",
+    in_out: "i",
+    stationID: "station001",
+    dateTime: "03-05-2018 10:00:00"
+  },
+  {
+    RFID: "rfid_dummy_002",
+    carID: "car001",
+    in_out: "o",
+    stationID: "station002",
+    dateTime: "03-05-2018 10:00:05"
+  },
+  {
+    RFID: "rfid_dummy_003",
+    carID: "car001",
+    in_out: "i",
+    stationID: "station001",
+    dateTime: "03-05-2018 10:00:00"
+  },
+  {
+    RFID: "rfid_dummy_003",
+    carID: "car001",
+    in_out: "o",
+    stationID: "station003",
+    dateTime: "03-05-2018 10:00:10"
+  },
+  {
+    RFID: "rfid_dummy_004",
+    carID: "car001",
+    in_out: "i",
+    stationID: "station002",
+    dateTime: "03-05-2018 10:00:05"
+  },
+  {
+    RFID: "rfid_dummy_004",
+    carID: "car001",
+    in_out: "o",
+    stationID: "station004",
+    dateTime: "03-05-2018 10:00:15"
+  },
+  {
+    RFID: "rfid_dummy_005",
+    carID: "car001",
+    in_out: "i",
+    stationID: "station002",
+    dateTime: "03-05-2018 10:00:05"
+  },
+  {
+    RFID: "rfid_dummy_005",
+    carID: "car001",
+    in_out: "o",
+    stationID: "station003",
+    dateTime: "03-05-2018 10:00:10"
+  },
+  {
+    RFID: "rfid_dummy_006",
+    carID: "car001",
+    in_out: "i",
+    stationID: "station003",
+    dateTime: "03-05-2018 10:00:10"
+  },
+  {
+    RFID: "rfid_dummy_006",
+    carID: "car001",
+    in_out: "o",
+    stationID: "station002",
+    dateTime: "03-05-2018 10:00:25"
+  },
+  {
+    RFID: "rfid_dummy_007",
+    carID: "car001",
+    in_out: "i",
+    stationID: "station003",
+    dateTime: "03-05-2018 10:00:10"
+  },
+  {
+    RFID: "rfid_dummy_007",
+    carID: "car001",
+    in_out: "o",
+    stationID: "station001",
+    dateTime: "03-05-2018 10:00:20"
+  },
+  {
+    RFID: "rfid_dummy_008",
+    carID: "car001",
+    in_out: "i",
+    stationID: "station001",
+    dateTime: "03-05-2018 10:00:05"
+  },
+  {
+    RFID: "rfid_dummy_008",
+    carID: "car001",
+    in_out: "o",
+    stationID: "station001",
+    dateTime: "03-05-2018 10:00:20"
+  },
+  {
+    RFID: "rfid_dummy_009",
+    carID: "car001",
+    in_out: "i",
+    stationID: "station003",
+    dateTime: "03-05-2018 10:00:10"
+  },
+  {
+    RFID: "rfid_dummy_009",
+    carID: "car001",
+    in_out: "o",
+    stationID: "station001",
+    dateTime: "03-05-2018 10:00:20"
+  },
+  {
+    RFID: "rfid_dummy_010",
+    carID: "car001",
+    in_out: "i",
+    stationID: "station004",
+    dateTime: "03-05-2018 10:00:15"
+  },
+  {
+    RFID: "rfid_dummy_010",
+    carID: "car001",
+    in_out: "o",
+    stationID: "station001",
+    dateTime: "03-05-2018 10:00:20"
+  },
+  // ============= CAR 002 ==============
+  {
+    RFID: "rfid_dummy_011",
+    carID: "car002",
+    in_out: "i",
+    stationID: "station002",
+    dateTime: "03-05-2018 10:00:05"
+  },
+  {
+    RFID: "rfid_dummy_011",
+    carID: "car002",
+    in_out: "o",
+    stationID: "station003",
+    dateTime: "03-05-2018 10:00:10"
+  },
+  {
+    RFID: "rfid_dummy_012",
+    carID: "car002",
+    in_out: "i",
+    stationID: "station002",
+    dateTime: "03-05-2018 10:00:05"
+  },
+  {
+    RFID: "rfid_dummy_012",
+    carID: "car002",
+    in_out: "o",
+    stationID: "station003",
+    dateTime: "03-05-2018 10:00:10"
+  },
+  {
+    RFID: "rfid_dummy_013",
+    carID: "car002",
+    in_out: "i",
+    stationID: "station002",
+    dateTime: "03-05-2018 10:00:05"
+  },
+  {
+    RFID: "rfid_dummy_013",
+    carID: "car002",
+    in_out: "o",
+    stationID: "station004",
+    dateTime: "03-05-2018 10:00:15"
+  },
+  {
+    RFID: "rfid_dummy_014",
+    carID: "car002",
+    in_out: "i",
+    stationID: "station003",
+    dateTime: "03-05-2018 10:00:10"
+  },
+  {
+    RFID: "rfid_dummy_014",
+    carID: "car002",
+    in_out: "o",
+    stationID: "station004",
+    dateTime: "03-05-2018 10:00:15"
+  },
+  {
+    RFID: "rfid_dummy_015",
+    carID: "car002",
+    in_out: "i",
+    stationID: "station003",
+    dateTime: "03-05-2018 10:00:10"
+  },
+  {
+    RFID: "rfid_dummy_015",
+    carID: "car002",
+    in_out: "o",
+    stationID: "station002",
+    dateTime: "03-05-2018 10:00:20"
+  },
+  {
+    RFID: "rfid_dummy_016",
+    carID: "car002",
+    in_out: "i",
+    stationID: "station004",
+    dateTime: "03-05-2018 10:00:15"
+  },
+  {
+    RFID: "rfid_dummy_016",
+    carID: "car002",
+    in_out: "o",
+    stationID: "station002",
+    dateTime: "03-05-2018 10:00:20"
+  },
+  {
+    RFID: "rfid_dummy_017",
+    carID: "car002",
+    in_out: "i",
+    stationID: "station004",
+    dateTime: "03-05-2018 10:00:15"
+  },
+  {
+    RFID: "rfid_dummy_017",
+    carID: "car002",
+    in_out: "o",
+    stationID: "station003",
+    dateTime: "03-05-2018 10:00:25"
+  },
+  {
+    RFID: "rfid_dummy_018",
+    carID: "car002",
+    in_out: "i",
+    stationID: "station004",
+    dateTime: "03-05-2018 10:00:15"
+  },
+  {
+    RFID: "rfid_dummy_018",
+    carID: "car002",
+    in_out: "o",
+    stationID: "station003",
+    dateTime: "03-05-2018 10:00:25"
+  },
+  {
+    RFID: "rfid_dummy_019",
+    carID: "car002",
+    in_out: "i",
+    stationID: "station002",
+    dateTime: "03-05-2018 10:00:05"
+  },
+  {
+    RFID: "rfid_dummy_019",
+    carID: "car002",
+    in_out: "o",
+    stationID: "station004",
+    dateTime: "03-05-2018 10:00:15"
+  },
+  {
+    RFID: "rfid_dummy_020",
+    carID: "car002",
+    in_out: "i",
+    stationID: "station003",
+    dateTime: "03-05-2018 10:00:10"
+  },
+  {
+    RFID: "rfid_dummy_020",
+    carID: "car002",
+    in_out: "o",
+    stationID: "station002",
+    dateTime: "03-05-2018 10:00:20"
+  },
+  // =============== CAR 003 ===================
+  {
+    RFID: "rfid_dummy_021",
+    carID: "car003",
+    in_out: "i",
+    stationID: "station003",
+    dateTime: "03-05-2018 10:00:10"
+  },
+  {
+    RFID: "rfid_dummy_021",
+    carID: "car003",
+    in_out: "o",
+    stationID: "station002",
+    dateTime: "03-05-2018 10:00:15"
+  },
+  {
+    RFID: "rfid_dummy_022",
+    carID: "car003",
+    in_out: "i",
+    stationID: "station003",
+    dateTime: "03-05-2018 10:00:10"
+  },
+  {
+    RFID: "rfid_dummy_022",
+    carID: "car003",
+    in_out: "o",
+    stationID: "station002",
+    dateTime: "03-05-2018 10:00:15"
+  },
+  {
+    RFID: "rfid_dummy_023",
+    carID: "car003",
+    in_out: "i",
+    stationID: "station003",
+    dateTime: "03-05-2018 10:00:10"
+  },
+  {
+    RFID: "rfid_dummy_023",
+    carID: "car005",
+    in_out: "o",
+    stationID: "station002",
+    dateTime: "03-05-2018 10:00:20"
+  },
+  {
+    RFID: "rfid_dummy_024",
+    carID: "car003",
+    in_out: "i",
+    stationID: "station003",
+    dateTime: "03-05-2018 10:00:10"
+  },
+  {
+    RFID: "rfid_dummy_024",
+    carID: "car003",
+    in_out: "o",
+    stationID: "station005",
+    dateTime: "03-05-2018 10:00:20"
+  },
+  {
+    RFID: "rfid_dummy_025",
+    carID: "car003",
+    in_out: "i",
+    stationID: "station002",
+    dateTime: "03-05-2018 10:00:15"
+  },
+  {
+    RFID: "rfid_dummy_025",
+    carID: "car003",
+    in_out: "o",
+    stationID: "station005",
+    dateTime: "03-05-2018 10:00:20"
+  },
+  {
+    RFID: "rfid_dummy_026",
+    carID: "car003",
+    in_out: "i",
+    stationID: "station002",
+    dateTime: "03-05-2018 10:00:15"
+  },
+  {
+    RFID: "rfid_dummy_026",
+    carID: "car003",
+    in_out: "o",
+    stationID: "station005",
+    dateTime: "03-05-2018 10:00:20"
+  },
+  {
+    RFID: "rfid_dummy_027",
+    carID: "car003",
+    in_out: "i",
+    stationID: "station002",
+    dateTime: "03-05-2018 10:00:15"
+  },
+  {
+    RFID: "rfid_dummy_027",
+    carID: "car003",
+    in_out: "o",
+    stationID: "station005",
+    dateTime: "03-05-2018 10:00:20"
+  },
+  {
+    RFID: "rfid_dummy_028",
+    carID: "car003",
+    in_out: "i",
+    stationID: "station002",
+    dateTime: "03-05-2018 10:00:15"
+  },
+  {
+    RFID: "rfid_dummy_028",
+    carID: "car003",
+    in_out: "o",
+    stationID: "station005",
+    dateTime: "03-05-2018 10:00:20"
+  },
+  {
+    RFID: "rfid_dummy_029",
+    carID: "car003",
+    in_out: "i",
+    stationID: "station005",
+    dateTime: "03-05-2018 10:00:20"
+  },
+  {
+    RFID: "rfid_dummy_029",
+    carID: "car003",
+    in_out: "o",
+    stationID: "station002",
+    dateTime: "03-05-2018 10:00:30"
+  },
+  {
+    RFID: "rfid_dummy_030",
+    carID: "car003",
+    in_out: "i",
+    stationID: "station005",
+    dateTime: "03-05-2018 10:00:20"
+  },
+  {
+    RFID: "rfid_dummy_030",
+    carID: "car003",
+    in_out: "o",
+    stationID: "station003",
+    dateTime: "03-05-2018 10:00:25"
+  },
 ]
 
 
--- Dummy-Data/STUDENT_DATA.js
+++ Dummy-Data/STUDENT_DATA.js
@@ -21,7 +21,8 @@ var STUDENT_DATA = [
   },
   {
     RFID: "rfid_dummy_005",
-    firstName: "Susan"
+    firstName: "Susan",
+    lastName: "Hunter"
   },
   {
     RFID: "rfid_dummy_006",
@@ -30,7 +31,8 @@ var STUDENT_DATA = [
   },
   {
     RFID: "rfid_dummy_007",
-    firstName: "Man"
+    firstName: "Man",
+    lastName: "Some"
   },
   {
     RFID: "rfid_dummy_008",
@@ -46,8 +48,108 @@ var STUDENT_DATA = [
     RFID: "rfid_dummy_010",
     firstName: "Dona",
     lastName: "Thumb"
+  },
+  {
+    RFID: "rfid_dummy_011",
+    firstName: "Riki",
+    lastName: "Maru"
+  },
+  {
+    RFID: "rfid_dummy_012",
+    firstName: "Bara",
+    lastName: "Thum"
+  },
+  {
+    RFID: "rfid_dummy_013",
+    firstName: "Shadow",
+    lastName: "Fiend"
+  },
+  {
+    RFID: "rfid_dummy_014",
+    firstName: "Invoker",
+    lastName: "Kael"
+  },
+  {
+    RFID: "rfid_dummy_015",
+    firstName: "Sand",
+    lastName: "King"
+  },
+  {
+    RFID: "rfid_dummy_016",
+    firstName: "Ru",
+    lastName: "Bick"
+  },
+  {
+    RFID: "rfid_dummy_017",
+    firstName: "Chaos",
+    lastName: "Knight"
+  },
+  {
+    RFID: "rfid_dummy_018",
+    firstName: "Dragon",
+    lastName: "Knight"
+  },
+  {
+    RFID: "rfid_dummy_019",
+    firstName: "Shadow",
+    lastName: "Daemon"
+  },
+  {
+    RFID: "rfid_dummy_020",
+    firstName: "Timber",
+    lastName: "Saw"
+  },
+  {
+    RFID: "rfid_dummy_021",
+    firstName: "Alax",
+    lastName: "Sander"
+  },
+  {
+    RFID: "rfid_dummy_022",
+    firstName: "Thunder",
+    lastName: "Bow"
+  },
+  {
+    RFID: "rfid_dummy_023",
+    firstName: "Poker",
+    lastName: "Card"
+  },
+  {
+    RFID: "rfid_dummy_024",
+    firstName: "Bicycle",
+    lastName: "Corner"
+  },
+  {
+    RFID: "rfid_dummy_025",
+    firstName: "Acer",
+    lastName: "Aspire"
+  },
+  {
+    RFID: "rfid_dummy_026",
+    firstName: "Asus",
+    lastName: "Zenfone"
+  },
+  {
+    RFID: "rfid_dummy_027",
+    firstName: "Apple",
+    lastName: "Mac"
+  },
+  {
+    RFID: "rfid_dummy_028",
+    firstName: "Iphone",
+    lastName: "Sumsung"
+  },
+  {
+    RFID: "rfid_dummy_029",
+    firstName: "Book",
+    lastName: "Pen"
+  },
+  {
+    RFID: "rfid_dummy_030",
+    firstName: "Adam",
+    lastName: "Intel"
   }
 ]
-// 10 data
+// 30 data
 
 module.exports.STUDENT_DATA = STUDENT_DATA
