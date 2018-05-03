3ccbd837eef5e31e907feb8520b531d74a01d937
unknown <afumagic3@gmail.com>
2018-05-03 17:31:05 +0700

    lat-lng for station_data

--- Dummy-Data/STATION_DATA.js
+++ Dummy-Data/STATION_DATA.js
@@ -2,32 +2,32 @@ STATION_DATA = [
   {
     stationID: "station001",
     name: "เซเว่นในมอ",
-    lat: "-99.99",
-    lng: "-99.99"
+    lat: "8.64694",
+    lng: "99.89493"
   },
   {
     stationID: "station002",
     name: "อาคารไทยบุรี",
-    lat: "-99.99",
-    lng: "-99.99"
+    lat: "8.64579",
+    lng: "99.89746"
   },
   {
     stationID: "station003",
     name: "แลปคอมพิวเตอร์",
-    lat: "-99.99",
-    lng: "-99.99"
+    lat: "8.64254",
+    lng: "99.89627"
   },
   {
     stationID: "station004",
     name: "ตึกสถาปัตย์",
-    lat: "-99.99",
-    lng: "-99.99"
+    lat: "8.64479",
+    lng: "99.89365"
   },
   {
     stationID: "station005",
     name: "ตึกวิจัย",
-    lat: "-99.99",
-    lng: "-99.99"
+    lat: "8.6401",
+    lng: "99.89944"
   }
 ]
 // 5 data
