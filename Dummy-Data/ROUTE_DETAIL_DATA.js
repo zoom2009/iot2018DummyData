255573923dfa7e51d4ae61fa1b647708e188b609
unknown <afumagic3@gmail.com>
2018-05-04 14:23:26 +0700

    fix route_detail dummy

--- Dummy-Data/ROUTE_DETAIL_DATA.js
+++ Dummy-Data/ROUTE_DETAIL_DATA.js
@@ -1,4 +1,5 @@
 var ROUTE_DETAIL_DATA = [
+  // =============== CAR1 ===================
   {
     carID: "car001",
     index: "1",
@@ -8,57 +9,197 @@ var ROUTE_DETAIL_DATA = [
   {
     carID: "car001",
     index: "2",
-    from: "station002",
+    from: "station001",
     to: "station003"
   },
   {
     carID: "car001",
     index: "3",
-    from: "station003",
-    to: "station004",
+    from: "station001",
+    to: "station004"
   },
   {
     carID: "car001",
     index: "4",
-    from: "station004",
-    to: "station001",
+    from: "station002",
+    to: "station001"
   },
+  {
+    carID: "car001",
+    index: "5",
+    from: "station002",
+    to: "station003"
+  },
+  {
+    carID: "car001",
+    index: "6",
+    from: "station002",
+    to: "station004"
+  },
+  {
+    carID: "car001",
+    index: "7",
+    from: "station003",
+    to: "station001"
+  },
+  {
+    carID: "car001",
+    index: "8",
+    from: "station003",
+    to: "station002"
+  },
+  {
+    carID: "car001",
+    index: "9",
+    from: "station003",
+    to: "station004"
+  },
+  {
+    carID: "car001",
+    index: "10",
+    from: "station004",
+    to: "station001"
+  },
+  {
+    carID: "car001",
+    index: "11",
+    from: "station004",
+    to: "station002"
+  },
+  {
+    carID: "car001",
+    index: "12",
+    from: "station004",
+    to: "station003"
+  },
+  {
+    carID: "car001",
+    index: "13",
+    from: "station001",
+    to: "station001"
+  },
+  {
+    carID: "car001",
+    index: "14",
+    from: "station002",
+    to: "station002"
+  },
+  {
+    carID: "car001",
+    index: "15",
+    from: "station003",
+    to: "station003"
+  },
+  // =============== CAR2 ===================
   {
     carID: "car002",
     index: "1",
     from: "station002",
-    to: "station003",
+    to: "station003"
   },
   {
     carID: "car002",
     index: "2",
-    from: "station003",
-    to: "station004",
+    from: "station002",
+    to: "station004"
   },
   {
     carID: "car002",
     index: "3",
-    from: "station004",
-    to: "station002",
+    from: "station003",
+    to: "station002"
   },
+  {
+    carID: "car002",
+    index: "4",
+    from: "station003",
+    to: "station004"
+  },
+  {
+    carID: "car002",
+    index: "5",
+    from: "station004",
+    to: "station002"
+  },
+  {
+    carID: "car002",
+    index: "6",
+    from: "station004",
+    to: "station003"
+  },
+  {
+    carID: "car002",
+    index: "7",
+    from: "station002",
+    to: "station002"
+  },
+  {
+    carID: "car002",
+    index: "8",
+    from: "station003",
+    to: "station003"
+  },
+  {
+    carID: "car002",
+    index: "9",
+    from: "station004",
+    to: "station004"
+  },
+  // =============== CAR3 ===================
   {
     carID: "car003",
     index: "1",
     from: "station003",
-    to: "station002",
+    to: "station002"
   },
   {
     carID: "car003",
     index: "2",
-    from: "station002",
-    to: "station005",
+    from: "station003",
+    to: "station005"
   },
   {
     carID: "car003",
     index: "3",
+    from: "station002",
+    to: "station003"
+  },
+  {
+    carID: "car003",
+    index: "4",
+    from: "station002",
+    to: "station005"
+  },
+  {
+    carID: "car003",
+    index: "5",
     from: "station005",
-    to: "station003",
-  }
+    to: "station003"
+  },
+  {
+    carID: "car003",
+    index: "6",
+    from: "station005",
+    to: "station002"
+  },
+  {
+    carID: "car003",
+    index: "7",
+    from: "station003",
+    to: "station003"
+  },
+  {
+    carID: "car003",
+    index: "8",
+    from: "station002",
+    to: "station002"
+  },
+  {
+    carID: "car003",
+    index: "9",
+    from: "station005",
+    to: "station005"
+  },
 ]
 
 module.exports.ROUTE_DETAIL_DATA = ROUTE_DETAIL_DATA
