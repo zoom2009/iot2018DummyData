01ccc9459d55b78a61d4949fd4c7e89d9a89edfd
unknown <afumagic3@gmail.com>
2018-05-04 12:08:41 +0700

    210 student dummy

--- Dummy-Data/STUDENT_DATA.js
+++ Dummy-Data/STUDENT_DATA.js
@@ -150,507 +150,908 @@ var STUDENT_DATA = [
     lastName: "Intel"
   },
   {
-	  RFID: "rfid_dummy_031"
+	  RFID: "rfid_dummy_031",
 	  firstName: "Aileen",
 	  lastName: "Lee"
   },
   {
-  	RFID: "rfid_dummy_032"
+  	RFID: "rfid_dummy_032",
   	firstName: "Ashira",
   	lastName: "Sawada"
   },
   {
-  	RFID: "rfid_dummy_033"
+  	RFID: "rfid_dummy_033",
   	firstName: "Akira",
   	lastName: "Soho"
   },
   {
-  	RFID: "rfid_dummy_034"
+  	RFID: "rfid_dummy_034",
   	firstName: "Bolin",
   	lastName: "Xia"
   },
   {
-  	RFID: "rfid_dummy_035"
+  	RFID: "rfid_dummy_035",
   	firstName: "Liu",
   	lastName: "Shishi"
   },
   {
-  	RFID: "rfid_dummy_036"
+  	RFID: "rfid_dummy_036",
   	firstName: "Yui",
   	lastName: "Aragaki"
   },
   {
-  	RFID: "rfid_dummy_037"
+  	RFID: "rfid_dummy_037",
   	firstName: "Haruka",
   	lastName: "Ayase"
   },
   {
-  	RFID: "rfid_dummy_038"
+  	RFID: "rfid_dummy_038",
   	firstName: "Tiffany",
   	lastName: "Zhuo"
   },
   {
-  	RFID: "rfid_dummy_039"
+  	RFID: "rfid_dummy_039",
   	firstName: "Miura",
   	lastName: "Tanaka"
   },
   {
-  	RFID: "rfid_dummy_040"
+  	RFID: "rfid_dummy_040",
   	firstName: "Kai",
   	lastName: "Yamamoto"
   },
   {
-  	RFID: "rfid_dummy_041"
+  	RFID: "rfid_dummy_041",
   	firstName: "Mei",
   	lastName: "Kato"
   },
   {
-  	RFID: "rfid_dummy_042"
+  	RFID: "rfid_dummy_042",
   	firstName: "Saburi",
   	lastName: "Sasaki"
   },
   {
-  	RFID: "rfid_dummy_043"
+  	RFID: "rfid_dummy_043",
   	firstName: "Ken",
   	lastName: "Matsumoto"
   },
   {
-  	RFID: "rfid_dummy_044"
+  	RFID: "rfid_dummy_044",
   	firstName: "Ren",
   	lastName: "Nielsen"
   },
   {
-  	RFID: "rfid_dummy_045"
+  	RFID: "rfid_dummy_045",
   	firstName: "Mark",
   	lastName: "Pedersen"
   },
   {
-  	RFID: "rfid_dummy_046"
+  	RFID: "rfid_dummy_046",
   	firstName: "Thomas",
   	lastName: "Taylor"
   },
   {
-  	RFID: "rfid_dummy_047"
+  	RFID: "rfid_dummy_047",
   	firstName: "Jan",
   	lastName: "Petit"
   },
   {
-  	RFID: "rfid_dummy_048"
+  	RFID: "rfid_dummy_048",
   	firstName: "Ahmed",
   	lastName: "Ali"
   },
   {
-  	RFID: "rfid_dummy_049"
+  	RFID: "rfid_dummy_049",
   	firstName: "Kyoko",
   	lastName: "Fukuda"
   },
   {
-  	RFID: "rfid_dummy_050"
+  	RFID: "rfid_dummy_050",
   	firstName: "Ryo",
   	lastName: "Nishikido"
   },
   {
-	  RFID: "rfid_dummy_051"
+	  RFID: "rfid_dummy_051",
 	  firstName: "Mani",
 	  lastName: "Srisuk"
   },
   {
-  	RFID: "rfid_dummy_052"
+  	RFID: "rfid_dummy_052",
   	firstName: "Somsri",
   	lastName: "Pakdang"
   },
   {
-  	RFID: "rfid_dummy_053"
+  	RFID: "rfid_dummy_053",
   	firstName: "Somchai",
   	lastName: "Suksabay"
   },
   {
-  	RFID: "rfid_dummy_054"
+  	RFID: "rfid_dummy_054",
   	firstName: "Sompong",
   	lastName: "Sorlab"
   },
   {
-  	RFID: "rfid_dummy_055"
+  	RFID: "rfid_dummy_055",
   	firstName: "Somtuy",
   	lastName: "Plamik"
   },
   {
-  	RFID: "rfid_dummy_056"
+  	RFID: "rfid_dummy_056",
   	firstName: "Somruk",
   	lastName: "Kumsing"
   },
   {
-  	RFID: "rfid_dummy_057"
+  	RFID: "rfid_dummy_057",
   	firstName: "Somman",
   	lastName: "Keawklad"
   },
   {
-  	RFID: "rfid_dummy_058"
+  	RFID: "rfid_dummy_058",
   	firstName: "Somjit",
   	lastName: "Jongjoho"
   },
   {
-  	RFID: "rfid_dummy_059"
+  	RFID: "rfid_dummy_059",
   	firstName: "Kongpob",
   	lastName: "Namdee"
   },
   {
-  	RFID: "rfid_dummy_060"
+  	RFID: "rfid_dummy_060",
   	firstName: "Teeratorn",
   	lastName: "Bunmatan"
   },
   {
-  	RFID: "rfid_dummy_061"
+  	RFID: "rfid_dummy_061",
   	firstName: "Chanatip",
   	lastName: "Songasin"
   },
   {
-  	RFID: "rfid_dummy_062"
+  	RFID: "rfid_dummy_062",
   	firstName: "Kawin",
   	lastName: "Tammasutjanun"
   },
   {
-  	RFID: "rfid_dummy_063"
+  	RFID: "rfid_dummy_063",
   	firstName: "Napat",
   	lastName: "Pakdee"
   },
   {
-  	RFID: "rfid_dummy_064"
+  	RFID: "rfid_dummy_064",
   	firstName: "Chaiwat",
   	lastName: "Buran"
   },
   {
-  	RFID: "rfid_dummy_065"
+  	RFID: "rfid_dummy_065",
   	firstName: "Kerkrit",
   	lastName: "Taweekan"
   },
   {
-  	RFID: "rfid_dummy_066"
+  	RFID: "rfid_dummy_066",
   	firstName: "Charil",
   	lastName: "Subbuy"
   },
   {
-  	RFID: "rfid_dummy_067"
+  	RFID: "rfid_dummy_067",
   	firstName: "Teerasil",
   	lastName: "Dangda"
   },
   {
-  	RFID: "rfid_dummy_068"
+  	RFID: "rfid_dummy_068",
   	firstName: "Panut",
   	lastName: "Sansuk"
   },
   {
-  	RFID: "rfid_dummy_069"
+  	RFID: "rfid_dummy_069",
   	firstName: "Chuwit",
   	lastName: "Kamonwisit"
   },
   {
-  	RFID: "rfid_dummy_070"
+  	RFID: "rfid_dummy_070",
   	firstName: "Pansa",
   	lastName: "Hamwibul"
   },
   {
-	  RFID: "rfid_dummy_071"
+	  RFID: "rfid_dummy_071",
     firstName: "Meena",
 	  lastName: "Trasook"
   },
   {
-  	RFID: "rfid_dummy_072"
+  	RFID: "rfid_dummy_072",
   	firstName: "Sritanya",
   	lastName: "Mareng"
   },
   {
-  	RFID: "rfid_dummy_073"
+  	RFID: "rfid_dummy_073",
   	firstName: "Somcsak",
   	lastName: "Suksai"
   },
   {
-  	RFID: "rfid_dummy_074"
+  	RFID: "rfid_dummy_074",
   	firstName: "Somrak",
   	lastName: "Suwanmanee"
   },
   {
-  	RFID: "rfid_dummy_075"
+  	RFID: "rfid_dummy_075",
   	firstName: "Ruksa",
   	lastName: "Pinklao"
   },
   {
-  	RFID: "rfid_dummy_076"
+  	RFID: "rfid_dummy_076",
   	firstName: "Ruktor",
   	lastName: "Somrudee"
   },
   {
-  	RFID: "rfid_dummy_077"
+  	RFID: "rfid_dummy_077",
   	firstName: "Sompreaw",
   	lastName: "Wanjung"
   },
   {
-  	RFID: "rfid_dummy_078"
+  	RFID: "rfid_dummy_078",
   	firstName: "Piyanooch",
   	lastName: "Ramtuang"
   },
   {
-  	RFID: "rfid_dummy_079"
+  	RFID: "rfid_dummy_079",
   	firstName: "Asadawut",
   	lastName: "Yuthana"
   },
   {
-  	RFID: "rfid_dummy_080"
+  	RFID: "rfid_dummy_080",
   	firstName: "Afdorn",
   	lastName: "Jekoh"
   },
   {
-  	RFID: "rfid_dummy_081"
+  	RFID: "rfid_dummy_081",
   	firstName: "Akram",
   	lastName: "Mana"
   },
   {
-  	RFID: "rfid_dummy_082"
+  	RFID: "rfid_dummy_082",
   	firstName: "Chareef",
   	lastName: "Masanit"
   },
   {
-  	RFID: "rfid_dummy_083"
+  	RFID: "rfid_dummy_083",
   	firstName: "Afdul",
   	lastName: "Maha"
   },
   {
-  	RFID: "rfid_dummy_084"
+  	RFID: "rfid_dummy_084",
   	firstName: "Rusdee",
   	lastName: "Burung"
   },
   {
-  	RFID: "rfid_dummy_085"
+  	RFID: "rfid_dummy_085",
   	firstName: "Fatih",
   	lastName: "Darama"
   },
   {
-  	RFID: "rfid_dummy_086"
+  	RFID: "rfid_dummy_086",
   	firstName: "Adel",
   	lastName: "Kulem"
   },
   {
-  	RFID: "rfid_dummy_087"
+  	RFID: "rfid_dummy_087",
   	firstName: "Meena",
   	lastName: "Pasook"
   },
   {
-  	RFID: "rfid_dummy_088"
+  	RFID: "rfid_dummy_088",
   	firstName: "Adamas",
   	lastName: "Denji"
   },
   {
-  	RFID: "rfid_dummy_089"
+  	RFID: "rfid_dummy_089",
   	firstName: "Taksin",
   	lastName: "Chinawut"
   },
   {
-  	RFID: "rfid_dummy_090"
+  	RFID: "rfid_dummy_090",
   	firstName: "Poo",
   	lastName: "Pongsit"
   },
   {
-	  RFID: "rfid_dummy_091"
+	  RFID: "rfid_dummy_091",
 	  firstName: "Aadi",
 	  lastName: "Aalase"
   },
   {
-  	RFID: "rfid_dummy_092"
+  	RFID: "rfid_dummy_092",
   	firstName: "Abella",
   	lastName: "Abrial"
   },
   {
-  	RFID: "rfid_dummy_093"
+  	RFID: "rfid_dummy_093",
   	firstName: "Absolon",
   	lastName: "Aceline"
   },
   {
-  	RFID: "rfid_dummy_094"
+  	RFID: "rfid_dummy_094",
   	firstName: "Achille",
   	lastName: "Achille"
   },
   {
-  	RFID: "rfid_dummy_095"
+  	RFID: "rfid_dummy_095",
   	firstName: "Adalicia",
   	lastName: "Adalie"
   },
   {
-  	RFID: "rfid_dummy_096"
+  	RFID: "rfid_dummy_096",
   	firstName: "Adalyn",
   	lastName: "Adelia"
   },
   {
-  	RFID: "rfid_dummy_097"
+  	RFID: "rfid_dummy_097",
   	firstName: "Adeline",
   	lastName: "Adelisa"
   },
   {
-  	RFID: "rfid_dummy_098"
+  	RFID: "rfid_dummy_098",
   	firstName: "Adelise",
   	lastName: "Adelynn"
   },
   {
-  	RFID: "rfid_dummy_099"
+  	RFID: "rfid_dummy_099",
   	firstName: "Adilene",
   	lastName: "Adorlee"
   },
   {
-  	RFID: "rfid_dummy_100"
+  	RFID: "rfid_dummy_100",
   	firstName: "Adreanna",
   	lastName: "Adrianna"
   },
   {
-  	RFID: "rfid_dummy_101"
+  	RFID: "rfid_dummy_101",
   	firstName: "Adrianne",
   	lastName: "Adriene"
   },
   {
-  	RFID: "rfid_dummy_102"
+  	RFID: "rfid_dummy_102",
   	firstName: "Adrienne",
   	lastName: "Advent"
   },
   {
-  	RFID: "rfid_dummy_103"
+  	RFID: "rfid_dummy_103",
   	firstName: "Afrodille",
   	lastName: "Aiglentina"
   },
   {
-  	RFID: "rfid_dummy_104"
+  	RFID: "rfid_dummy_104",
   	firstName: "Aim",
   	lastName: "Alaina"
   },
   {
-  	RFID: "rfid_dummy_105"
+  	RFID: "rfid_dummy_105",
   	firstName: "Alaine",
   	lastName: "Alayna"
   },
   {
-  	RFID: "rfid_dummy_106"
+  	RFID: "rfid_dummy_106",
   	firstName: "Albaric",
   	lastName: "Aldrick"
   },
   {
-  	RFID: "rfid_dummy_107"
+  	RFID: "rfid_dummy_107",
   	firstName: "Aleron",
   	lastName: "Alette"
   },
   {
-  	RFID: "rfid_dummy_108"
+  	RFID: "rfid_dummy_108",
   	firstName: "Alexandre",
   	lastName: "Alexandrie"
   },
   {
-  	RFID: "rfid_dummy_109"
+  	RFID: "rfid_dummy_109",
   	firstName: "Alexandrine",
   	lastName: "Alezae"
   },
   {
-  	RFID: "rfid_dummy_110"
+  	RFID: "rfid_dummy_110",
   	firstName: "Algernon",
   	lastName: "Algie"
   },
   {
-	  RFID: "rfid_dummy_111"
+	  RFID: "rfid_dummy_111",
 	  firstName: "Bellanita",
 	  lastName: "Brown"
   },
   {
-  	RFID: "rfid_dummy_112"
+  	RFID: "rfid_dummy_112",
   	firstName: "B?atrice",
   	lastName: "Mabel"
   },
   {
-  	RFID: "rfid_dummy_113"
+  	RFID: "rfid_dummy_113",
   	firstName: "Benny",
   	lastName: "Marco"
   },
   {
-  	RFID: "rfid_dummy_114"
+  	RFID: "rfid_dummy_114",
   	firstName: "Betty",
   	lastName: "Marlis"
   },
   {
-  	RFID: "rfid_dummy_115"
+  	RFID: "rfid_dummy_115",
   	firstName: "Belle",
   	lastName: "gonna"
   },
   {
-  	RFID: "rfid_dummy_116"
+  	RFID: "rfid_dummy_116",
   	firstName: "Franken",
   	lastName: "Olus"
   },
   {
-  	RFID: "rfid_dummy_117"
+  	RFID: "rfid_dummy_117",
   	firstName: "Penta",
   	lastName: "Marbo"
   },
   {
-  	RFID: "rfid_dummy_118"
+  	RFID: "rfid_dummy_118",
   	firstName: "Russa",
   	lastName: "Ajin"
   },
   {
-  	RFID: "rfid_dummy_119"
+  	RFID: "rfid_dummy_119",
   	firstName: "Jinjin",
   	lastName: "Koris"
   },
   {
-  	RFID: "rfid_dummy_120"
+  	RFID: "rfid_dummy_120",
   	firstName: "Annalis",
   	lastName: "Wisky"
   },
   {
-  	RFID: "rfid_dummy_121"
+  	RFID: "rfid_dummy_121",
   	firstName: "sky",
   	lastName: "Jolis"
   },
   {
-  	RFID: "rfid_dummy_122"
+  	RFID: "rfid_dummy_122",
   	firstName: "Jassy",
   	lastName: "Lora"
   },
   {
-  	RFID: "rfid_dummy_123"
+  	RFID: "rfid_dummy_123",
   	firstName: "Tai",
   	lastName: "Mari"
   },
   {
-  	RFID: "rfid_dummy_124"
+  	RFID: "rfid_dummy_124",
   	firstName: "nicky",
   	lastName: "corrus"
   },
 
   {
-  	RFID: "rfid_dummy_125"
+  	RFID: "rfid_dummy_125",
   	firstName: "Margie",
   	lastName: "Bingo"
   },
   {
-  	RFID: "rfid_dummy_126"
+  	RFID: "rfid_dummy_126",
   	firstName: "Mona",
   	lastName: "Lisa"
   },
   {
-  	RFID: "rfid_dummy_127"
+  	RFID: "rfid_dummy_127",
   	firstName: "Ardas",
   	lastName: "Brisgit"
   },
   {
-  	RFID: "rfid_dummy_128"
+  	RFID: "rfid_dummy_128",
   	firstName: "Bill",
   	lastName: "Rallew"
   },
   {
-  	RFID: "rfid_dummy_129"
+  	RFID: "rfid_dummy_129",
   	firstName: "Nanna",
   	lastName: "York"
   },
   {
-  	RFID: "rfid_dummy_130"
+  	RFID: "rfid_dummy_130",
   	firstName: "Nicha",
   	lastName: "Meka"
   },
+  {
+	  RFID: "rfid_dummy_131",
+    firstName: "Steven",
+	  lastName: "Gerrard"
+  },
+  {
+  	RFID: "rfid_dummy_132",
+  	firstName: "Fernado",
+  	lastName: "Torres"
+  },
+  {
+  	RFID: "rfid_dummy_133",
+  	firstName: "Jordan",
+  	lastName: "Henderson"
+  },
+  {
+  	RFID: "rfid_dummy_134",
+  	firstName: "Roberto",
+  	lastName: "Firmino"
+  },
+  {
+  	RFID: "rfid_dummy_135",
+  	firstName: "Alex",
+  	lastName: "Robert"
+  },
+  {
+  	RFID: "rfid_dummy_136",
+  	firstName: "Neil",
+  	lastName: "Robertson"
+  },
+  {
+  	RFID: "rfid_dummy_137",
+  	firstName: "James",
+  	lastName: "Milner"
+  },
+  {
+  	RFID: "rfid_dummy_138",
+  	firstName: "Xabi",
+  	lastName: "Alonso"
+  },
+  {
+  	RFID: "rfid_dummy_139",
+  	firstName: "Rafhael",
+  	lastName: "Vararn"
+  },
+  {
+  	RFID: "rfid_dummy_140",
+  	firstName: "Adam",
+  	lastName: "Johnson"
+  },
+  {
+  	RFID: "rfid_dummy_141",
+  	firstName: "Mohammed",
+  	lastName: "Salah"
+  },
+  {
+  	RFID: "rfid_dummy_142",
+  	firstName: "Sadio",
+  	lastName: "Mane"
+  },
+  {
+  	RFID: "rfid_dummy_143",
+  	firstName: "Judd",
+  	lastName: "Trum"
+  },
+  {
+  	RFID: "rfid_dummy_144",
+  	firstName: "Danny",
+  	lastName: "Ing"
+  },
+  {
+  	RFID: "rfid_dummy_145",
+  	firstName: "Alberto",
+  	lastName: "Moreno"
+  },
+  {
+  	RFID: "rfid_dummy_146",
+  	firstName: "Dominic",
+  	lastName: "Solanke"
+  },
+  {
+  	RFID: "rfid_dummy_147",
+  	firstName: "Ben",
+  	lastName: "Woodbern"
+  },
+  {
+  	RFID: "rfid_dummy_148",
+  	firstName: "Joe",
+  	lastName: "Gomez"
+  },
+  {
+  	RFID: "rfid_dummy_149",
+  	firstName: "Harry",
+  	lastName: "Kane"
+  },
+  {
+  	RFID: "rfid_dummy_150",
+  	firstName: "Christian",
+  	lastName: "Eriksen"
+  },
+  {
+	  RFID: "rfid_dummy_151",
+	  firstName: "Harry",
+	  lastName: "Dylan"
+  },
+  {
+  	RFID: "rfid_dummy_152",
+  	firstName: "Oliver",
+  	lastName: "Seth"
+  },
+  {
+  	RFID: "rfid_dummy_153",
+  	firstName: "Jack",
+  	lastName: "Alexander"
+  },
+  {
+  	RFID: "rfid_dummy_154",
+  	firstName: "Alfie",
+  	lastName: "Archie"
+  },
+  {
+  	RFID: "rfid_dummy_155",
+  	firstName: "Charlie",
+  	lastName: "Benjamin"
+  },
+  {
+  	RFID: "rfid_dummy_156",
+  	firstName: "Thomas",
+  	lastName: "Adams"
+  },
+  {
+  	RFID: "rfid_dummy_157",
+  	firstName: "Jacob",
+  	lastName: "Alexander"
+  },
+  {
+  	RFID: "rfid_dummy_158",
+  	firstName: "James",
+  	lastName: "Anderson"
+  },
+  {
+  	RFID: "rfid_dummy_159",
+  	firstName: "Joshua",
+  	lastName: "Beckham"
+  },
+  {
+  	RFID: "rfid_dummy_160",
+  	firstName: "William",
+  	lastName: "Baker"
+  },
+  {
+  	RFID: "rfid_dummy_161",
+  	firstName: "Ethan",
+  	lastName: "Bennett"
+  },
+  {
+  	RFID: "rfid_dummy_162",
+  	firstName: "George",
+  	lastName: "Campbell"
+  },
+  {
+  	RFID: "rfid_dummy_163",
+  	firstName: "Riley",
+  	lastName: "Carter"
+  },
+  {
+  	RFID: "rfid_dummy_164",
+  	firstName: "Daniel",
+  	lastName: "Collins"
+  },
+  {
+  	RFID: "rfid_dummy_165",
+  	firstName: "Samuel",
+  	lastName: "Edwards"
+  },
+  {
+  	RFID: "rfid_dummy_166",
+  	firstName: "Noah",
+  	lastName: "Evans"
+  },
+  {
+  	RFID: "rfid_dummy_167",
+  	firstName: "Oscar",
+  	lastName: "Gonzalez"
+  },
+  {
+  	RFID: "rfid_dummy_168",
+  	firstName: "Joseph",
+  	lastName: "Henderson"
+  },
+  {
+  	RFID: "rfid_dummy_169",
+  	firstName: "Aidan",
+  	lastName: "Harris"
+  },
+  {
+  	RFID: "rfid_dummy_170",
+  	firstName: "Max",
+  	lastName: "Howard"
+  },
+  {
+  	RFID: "rfid_dummy_171",
+  	firstName: "Lucas",
+  	lastName: "Jackson"
+  },
+  {
+  	RFID: "rfid_dummy_172",
+  	firstName: "Leo",
+  	lastName: "Johnson"
+  },
+  {
+  	RFID: "rfid_dummy_173",
+  	firstName: "Henry",
+  	lastName: "Kelly"
+  },
+  {
+  	RFID: "rfid_dummy_174",
+  	firstName: "Jake",
+  	lastName: "Miller"
+  },
+  {
+  	RFID: "rfid_dummy_175",
+  	firstName: "Logan",
+  	lastName: "Peterson"
+  },
+  {
+  	RFID: "rfid_dummy_176",
+  	firstName: "Adam",
+  	lastName: "Powell"
+  },
+  {
+  	RFID: "rfid_dummy_177",
+  	firstName: "Harrison",
+  	lastName: "Roberts"
+  },
+  {
+  	RFID: "rfid_dummy_178",
+  	firstName: "Lewis",
+  	lastName: "Rogers"
+  },
+  {
+  	RFID: "rfid_dummy_179",
+  	firstName: "Edward",
+  	lastName: "Stewart"
+  },
+  {
+  	RFID: "rfid_dummy_180",
+  	firstName: "Freddie",
+  	lastName: "Robinson"
+  },
+  {
+  	RFID: "rfid_dummy_181",
+  	firstName: "Matthew",
+  	lastName: "Perry"
+  },
+  {
+  	RFID: "rfid_dummy_182",
+  	firstName: "Liam",
+  	lastName: "Peretz"
+  },
+  {
+  	RFID: "rfid_dummy_183",
+  	firstName: "Zachary",
+  	lastName: "Parker"
+  },
+  {
+  	RFID: "rfid_dummy_184",
+  	firstName: "Sebastian",
+  	lastName: "Nelson"
+  },
+
+  {
+  	RFID: "rfid_dummy_185",
+  	firstName: "Ryan",
+  	lastName: "Morris"
+  },
+  {
+  	RFID: "rfid_dummy_186",
+  	firstName: "Mason",
+  	lastName: "Morgan"
+  },
+  {
+  	RFID: "rfid_dummy_187",
+  	firstName: "Finley",
+  	lastName: "Martinez"
+  },
+  {
+  	RFID: "rfid_dummy_188",
+  	firstName: "Isaac",
+  	lastName: "Martin"
+  },
+  {
+  	RFID: "rfid_dummy_189",
+  	firstName: "Jayden",
+  	lastName: "Lopez"
+  },
+  {
+  	RFID: "rfid_dummy_190",
+  	firstName: "Tyler",
+  	lastName: "Lewis"
+  },
+  {
+	  RFID: "rfid_dummy_191",
+	  firstName: "Laila",
+	  lastName: "tavee"
+  },
+  {
+  	RFID: "rfid_dummy_192",
+  	firstName: "Lilium",
+  	lastName: "Lee"
+  },
+  {
+  	RFID: "rfid_dummy_193",
+  	firstName: "Logan",
+  	lastName: "Marra"
+  },
+  {
+  	RFID: "rfid_dummy_194",
+  	firstName: "Lyla",
+  	lastName: "Lucky"
+  },
+  {
+  	RFID: "rfid_dummy_195",
+  	firstName: "Lenin",
+  	lastName: "Ronna"
+  },
+  {
+  	RFID: "rfid_dummy_196",
+  	firstName: "Lynda",
+  	lastName: "Overson"
+  },
+  {
+  	RFID: "rfid_dummy_197",
+  	firstName: "Lula",
+  	lastName: "Beena"
+  },
+  {
+  	RFID: "rfid_dummy_198",
+  	firstName: "Lotte",
+  	lastName: "Talos"
+  },
+  {
+  	RFID: "rfid_dummy_199",
+  	firstName: "Ser",
+  	lastName: "Yoris"
+  },
+  {
+  	RFID: "rfid_dummy_200",
+  	firstName: "Daran",
+  	lastName: "Meeka"
+  },
+  {
+  	RFID: "rfid_dummy_201",
+  	firstName: "Jenna",
+  	lastName: "Aga"
+  },
+  {
+  	RFID: "rfid_dummy_202",
+  	firstName: "Agus",
+  	lastName: "Thongmee"
+  },
+  {
+  	RFID: "rfid_dummy_203",
+  	firstName: "Tia",
+  	lastName: "Mari"
+  },
+  {
+  	RFID: "rfid_dummy_204",
+  	firstName: "Tanakorn",
+  	lastName: "Petsri"
+  },
+  {
+  	RFID: "rfid_dummy_205",
+  	firstName: "Tanu",
+  	lastName: "Bio"
+  },
+  {
+  	RFID: "rfid_dummy_206",
+  	firstName: "carror",
+  	lastName: "Tin"
+  },
+  {
+  	RFID: "rfid_dummy_207",
+  	firstName: "Maybe",
+  	lastName: "Punya"
+  },
+  {
+  	RFID: "rfid_dummy_208",
+  	firstName: "Sipa",
+  	lastName: "Petpeng"
+  },
+  {
+  	RFID: "rfid_dummy_209",
+  	firstName: "Wachara",
+  	lastName: "Pong"
+  },
+  {
+  	RFID: "rfid_dummy_210",
+  	firstName: "Apich",
+  	lastName: "Tantee"
+  }
 ]
-// 130 data
+// 210 data
 
 module.exports.STUDENT_DATA = STUDENT_DATA
